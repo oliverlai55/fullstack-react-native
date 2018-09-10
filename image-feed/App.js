5df5b7148466508b578c29fce63c4bfef2f45ddd
Oliver Lai <oliverlai55@gmail.com>
2018-09-10 01:39:00 -0400

    Add additional compoennts

--- image-feed/App.js
+++ image-feed/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Avatar from './components/Avatar';
 import { Constants } from 'expo';
 import AuthorRow from './components/AuthorRow';
 import Card from './components/Card';
+import CardList from './components/CardList';
 
-export default class App extends React.Component {
+const items = [
+  { id: 0, author: 'Bob Ross' },
+  { id, 1, author: 'Chuck Norris' },
+];
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Card 
-          fullname={'First Last'}
-          linkText={'Comments'}
-          onPressLinkText={() => {
-            console.log('Pressed link!');
-          }}
-          image={{ uri: 'https://unspash.it/600/600' }}
-        />
+        <CardList items={items} />
       </View>
     );
   }
