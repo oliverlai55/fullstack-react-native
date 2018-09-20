d2f674941c85a30882e0d79161d795958a88632c
Oliver Lai <oliverlai55@gmail.com>
2018-09-19 22:18:18 -0400

    Add all components in screen

--- image-feed/components/NavigationBar.js
+++ image-feed/components/NavigationBar.js
@@ -1,9 +1,16 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 
 export default function NavigationBar({ title, leftText, onPressLeftText }) {
-
+  return (
+    <View style={style.container}>
+      <TouchableOpacity style={styles.leftText} onPress={onPressLeftText}>
+        <Text>{leftText}</Text>
+      </TouchableOpacity>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
 }
 
 NavigationBar.propTypes = {
@@ -17,3 +24,23 @@ NavigationBar.defaultProps = {
   leftText: '',
   onPressLeftText: () => {},
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: 40,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: 'rgba(0, 0, 0, 0.1)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: '500',
+  },
+  leftText: {
+    position: 'absolute',
+    left: 20,
+    top: 0,
+    bottom: 0,
+    justifyContent: 'center',
+  },
+});
